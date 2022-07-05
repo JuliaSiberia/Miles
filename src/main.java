eb335c9fe09141d28dee7852f5d8804698a965da
Yuliya Kulpina <iyuliyaraspopina@gmail.com>
2022-07-05 17:16:07 +0800

    Добавлен расчет миль

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main  {
+    public static void main(String[] args) {
+        int price = 5000;
+        int bonus = 20;
+        int miles = price/bonus;
+
+        System.out.println("Начисленные мили " + miles);
+        
+    }
+}
